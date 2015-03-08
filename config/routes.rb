a155310b0d90ad0d28a8d42b225f6eba9c18246f
Shawn King <shawn_king_22@hotmail.com>
2015-03-07 20:04:22 -0500

    Creating Project page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root "pages#home"
   get "about" => "pages#about" # creates about_path
+  get "project" => "pages#project" # creates project_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
