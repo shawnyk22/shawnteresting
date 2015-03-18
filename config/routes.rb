07633b6d8237e7e03616351b424eff46b231f569
Shawn King <shawn_king_22@hotmail.com>
2015-03-18 15:25:55 -0400

    New User SIgnup and Signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
- get 'pages/home'
-
+  devise_for :users
   root "pages#home"
   get "about" => "pages#about" # creates about_path
   get "project" => "pages#project" # creates project_path
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
